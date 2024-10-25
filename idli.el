ad978f840528b573b2389bdb4a82f5d427cc8d6e
Abhinav Tushar <abhinav@lepisma.xyz>
2024-10-25 13:28:05 +0530

    v0.1.0

--- idli.el
+++ idli.el
@@ -3,7 +3,7 @@
 ;; Copyright (c) 2024 Abhinav Tushar
 
 ;; Author: Abhinav Tushar <abhinav@lepisma.xyz>
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29") (llm "0.17.2") (org "9.6.15"))
 ;; Keywords: llm, learning
 ;; URL: https://github.com/lepisma/idli
